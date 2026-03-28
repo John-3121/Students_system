5b24664210cfb75eee639fb6da12a6673601e52f
johncarlo <johncarlocabadsan@gmail.com>
2026-03-28 19:55:50 +0800

    added search student feature

--- main.java
+++ main.java
@@ -12,19 +12,21 @@ public class main{
 				System.out.println("1.Add student");
 				System.out.println("2.View all student");
 				System.out.println("3.remove student");
-				System.out.println("4.exit");
+				System.out.println("4.Update student");
+				System.out.println("5.Search student");
+				System.out.println("6.exit");
 				int user_input = sn.nextInt();
 				sn.nextLine();
 				
 				switch(user_input){
 					case 1:
 						try{
-							System.out.println("Enter your name");
+							System.out.println("Enter name");
 							String name = sn.nextLine();
-							System.out.println("Enter your age");
+							System.out.println("Enter age");
 							int age = sn.nextInt();
 							sn.nextLine();
-							System.out.println("Enter your course");
+							System.out.println("Enter course");
 							String course = sn.nextLine();
 							st_dao.addStudent(new student(name,age,course));
 							System.out.println("Added sucessfully");
@@ -51,6 +53,27 @@ public class main{
 					
 						break;
 					case 4:
+							System.out.println("Enter the id desired to update");
+							int id = sn.nextInt();
+							sn.nextLine();
+							System.out.println("Enter name");
+							String name = sn.nextLine();
+							System.out.println("Enter age");
+							int age = sn.nextInt();
+							sn.nextLine();
+							System.out.println("Enter course");
+							String course = sn.nextLine();
+							st_dao.update_student(id,new student(name,age,course));
+							System.out.println("Info updated sucessfully");
+							
+
+						break;
+					case 5:
+						System.out.println("search Student name");
+						String st_name = sn.nextLine();
+						st_dao.search_student(st_name);
+						break;
+					case 6:
 						exit=false;
 						System.out.println("bye");
 						break;
