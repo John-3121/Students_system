0de14669e7ad72383a8db4166cd5bb7629f7e3f7
John-3121 <johncarlocabadsan@gmail.com>
2026-04-24 17:06:20 +0800

    Update dbconnection.java

--- dbconnection.java
+++ dbconnection.java
@@ -5,7 +5,7 @@ import java.io.*;
 
 public class dbconnection{
 			private static final String url= "jdbc:mysql://localhost:3306/students_databsse";
-			private static final String uname="carline";
+			private static final String uname="john_carlo";
 			private static final String password="";
 	public static Connection dbcn() throws Exception{
 	        return DriverManager.getConnection(url,uname,password);
